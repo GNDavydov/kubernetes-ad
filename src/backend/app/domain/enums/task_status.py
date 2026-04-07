f0dfdc4033c17dd73352071fbfbec65e255b8fd3
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-07 19:14:02 +0300

    feat: обновил enum

--- src/backend/app/domain/enums/task_status.py
+++ src/backend/app/domain/enums/task_status.py
@@ -2,7 +2,9 @@ from enum import StrEnum
 
 
 class TaskStatus(StrEnum):
+    STARTING = "starting"
     PENDING = "pending"
     RUNNING = "running"
     DONE = "done"
     FAILED = "failed"
+    CANCELED = "canceled"
