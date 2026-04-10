ab0be6bebb4d2df3a03946058e1552745b3760cd
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-10 16:41:53 +0300

    fix: исправил проблему с init фалом

--- src/backend/app/infrastructure/db/models/__init__.py
+++ src/backend/app/infrastructure/db/models/__init__.py
@@ -2,14 +2,14 @@ from app.infrastructure.db.models.base import Base
 from app.infrastructure.db.models.integration import IntegrationModel
 from app.infrastructure.db.models.model import ModelModel
 from app.infrastructure.db.models.task import TaskModel
-from backend.app.infrastructure.db.models.detect_metric import TaskResultModel
-from backend.app.infrastructure.db.models.train_metric import TrainingMetricModel
+from app.infrastructure.db.models.detect_metric import DetectMetricModel
+from app.infrastructure.db.models.train_metric import TrainMetricModel
 
 __all__ = [
     "Base",
     "IntegrationModel",
     "ModelModel",
     "TaskModel",
-    "TaskResultModel",
-    "TrainingMetricModel",
+    "DetectMetricModel",
+    "TrainMetricModel",
 ]
