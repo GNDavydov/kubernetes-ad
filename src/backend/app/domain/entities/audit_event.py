5592180996a1789c8c0f5136645ef254f79049dc
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-15 15:25:40 +0300

    refactor: entities of auditevents

--- src/backend/app/domain/entities/audit_event.py
+++ src/backend/app/domain/entities/audit_event.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from typing import Any, Optional
 
 
-@dataclass
 class AuditEvent:
     timestamp: datetime
     verb: str
@@ -14,6 +12,28 @@ class AuditEvent:
     object_namespace: Optional[str]
     response_code: Optional[int]
     source_ips: list[str]
+
+    def __init__(self,
+                 timestamp: datetime,
+                 verb: str,
+                 user_username: str,
+                 user_agent: str,
+                 object_resource: str,
+                 object_subresource: Optional[str],
+                 object_namespace: Optional[str],
+                 response_code: Optional[int],
+                 source_ips: list[str]
+                 ) -> None:
+        self.timestamp = timestamp
+        self.verb = verb
+        self.user_username = user_username
+        self.user_agent = user_agent
+        self.object_resource = object_resource
+        self.object_subresource = object_subresource
+        self.object_namespace = object_namespace
+        self.response_code = response_code
+        self.source_ips = source_ips
+        self.vec = None
 
     def timestamp_as_datetime(self) -> datetime:
         if self.timestamp.tzinfo is None:
