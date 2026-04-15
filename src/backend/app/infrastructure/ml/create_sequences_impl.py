ab27fd21bd1b66235063979f2186f8b1691240b3
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-15 17:22:35 +0300

    feat: реализовал через numpy

--- src/backend/app/infrastructure/ml/create_sequences_impl.py
+++ src/backend/app/infrastructure/ml/create_sequences_impl.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from typing import List
 
+import numpy as np
+
 from app.domain.entities.audit_event import AuditEvent
 from app.domain.interfaces.create_sequences import CreateSequences
 from app.domain.interfaces.encode_pipeline import EncodePipeline
@@ -11,7 +13,7 @@ class CreateSequencesImpl(CreateSequences):
         self.encode_pipeline = encode_pipeline
         self.seq_len = seq_len
 
-    def transform(self, events: list[AuditEvent]) -> list[list]:
+    def transform(self, events: list[AuditEvent]) -> np.ndarray:
         grouped: dict[str, List[AuditEvent]] = defaultdict(list)
 
         for event in events:
@@ -22,12 +24,17 @@ class CreateSequencesImpl(CreateSequences):
                 )
             grouped[event.user_username].append(event)
 
-        sequences: List[list] = []
+        sequences = []
+
         for user_events in grouped.values():
             user_events.sort(key=lambda e: e.timestamp_as_datetime())
+            user_vecs = np.array([e.vec for e in user_events])
 
-            for i in range(len(user_events) - self.seq_len + 1):
-                window = user_events[i:i + self.seq_len]
-                sequences.append([e.vec for e in window])
+            if len(user_vecs) < self.seq_len:
+                continue
 
-        return sequences
+            for i in range(len(user_vecs) - self.seq_len + 1):
+                window = user_vecs[i:i + self.seq_len]
+                sequences.append(window)
+
+        return np.array(sequences)
