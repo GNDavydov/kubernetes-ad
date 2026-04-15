2ba66297f95e65f087f8ec0dc37fbc76491f6b45
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-15 15:55:51 +0300

    fix: db models

--- src/backend/app/infrastructure/db/models/__init__.py
+++ src/backend/app/infrastructure/db/models/__init__.py
@@ -1,14 +1,16 @@
 from app.infrastructure.db.models.base import Base
-from app.infrastructure.db.models.integration import IntegrationModel
+from app.infrastructure.db.models.user import UserModel
 from app.infrastructure.db.models.model import ModelModel
+from app.infrastructure.db.models.integration import IntegrationModel
 from app.infrastructure.db.models.task import TaskModel
 from app.infrastructure.db.models.detect_metric import DetectMetricModel
 from app.infrastructure.db.models.train_metric import TrainMetricModel
 
 __all__ = [
     "Base",
-    "IntegrationModel",
+    "UserModel",
     "ModelModel",
+    "IntegrationModel",
     "TaskModel",
     "DetectMetricModel",
     "TrainMetricModel",
