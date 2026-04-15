2da819582e59c6da0ad11bc3ccdff2d1c7bb7435
Давыдов Григорий Никитич <davy.grig.n@gmail.com>
2026-04-15 15:26:17 +0300

    fix: celery_queue

--- src/backend/app/infrastructure/task_queue/celery_app.py
+++ src/backend/app/infrastructure/task_queue/celery_app.py
@@ -1,5 +1,5 @@
-from uuid import UUID
 from typing import Callable
+from uuid import UUID
 
 from celery import Celery
 
@@ -7,7 +7,14 @@ from app.domain.interfaces.task_queue import TaskQueue
 
 
 class CeleryTaskQueue(TaskQueue):
-    def __init__(self, app_name: str, broker_url: str, backend_url: str, train_task_name: str = "app.train_task", detect_task_name: str = "app.detect_task") -> None:
+    def __init__(
+        self,
+        app_name: str,
+        broker_url: str,
+        backend_url: str,
+        train_task_name: str = "app.train_task",
+        detect_task_name: str = "app.detect_task",
+    ) -> None:
         self.app = Celery(
             app_name,
             broker=broker_url,
@@ -25,13 +32,13 @@ class CeleryTaskQueue(TaskQueue):
         self._detect_task = None
         self._train_task = None
 
-    def register_detect_task(self, detect_task: Callable[[UUID], None]):
-        task = self.app.task(name="app.detect_task")(detect_task)
+    def register_detect_task(self, detect_task: Callable[[UUID | str], None]):
+        task = self.app.task(name=self.detect_task_name)(detect_task)
         self._detect_task = task
         return task
 
-    def register_train_task(self, train_task: Callable[[UUID], None]):
-        task = self.app.task(name="app.train_task")(train_task)
+    def register_train_task(self, train_task: Callable[[UUID | str], None]):
+        task = self.app.task(name=self.train_task_name)(train_task)
         self._train_task = task
         return task
 
@@ -39,4 +46,4 @@ class CeleryTaskQueue(TaskQueue):
         self.app.send_task(self.detect_task_name, args=[task_id])
 
     def train_task(self, task_id: UUID) -> None:
-        self.app.send_task(self.detect_task_name, args=[task_id])
+        self.app.send_task(self.train_task_name, args=[task_id])
